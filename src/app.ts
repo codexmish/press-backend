c48e746c6ccdc9729d24b087007000cf41ce31f0
codexmish <majharulislam.shanto.70@gmail.com>
2026-06-21 13:24:59 +0600

    dontenv config done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express, { Application, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import config from "./config";
 
 const app: Application = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: config.app_url,
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import app from "./app";
-const port = 8000;
+import config from "./config";
+import { prisma } from "./lib/prisma";
+const port = config.port;
 async function main() {
   try {
+    // await prisma.$connect();
+    console.log("connected database");
+
     app.listen(port, () => {
       console.log(`server is running on port ${port}`);
     });
   } catch (error) {
     console.error("error starting the server:", error);
+    // await prisma.$disconnect();
     process.exit(1);
   }
 }
