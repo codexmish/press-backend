3c199a187bea7232b7669908cca48046d276fc60
codexmish <majharulislam.shanto.70@gmail.com>
2026-06-24 13:39:07 +0600

    jwt token generated

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,27 +1,44 @@
+import config from "../../config";
 import { prisma } from "../../lib/prisma";
+import { jwtUtils } from "../../utils/jwt";
 import { Logindata } from "./auth.interface";
-import brypt from "bcrypt"
-
+import brypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 // ---------login service
 const loginUser = async (payload: Logindata) => {
   const { email, password } = payload;
 
   const user = await prisma.user.findUniqueOrThrow({
-    where: {email}
-  })
+    where: { email },
+  });
 
-  
+  const isPasswordMatchaed = await brypt.compare(password, user.password);
 
-
-  const isPasswordMatchaed = await brypt.compare(password, user.password)
-
-  if(!isPasswordMatchaed){
-    throw new Error("invalid credantial")
+  if (!isPasswordMatchaed) {
+    throw new Error("invalid credantial");
   }
 
-  return user
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
 
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret!,
+    { expiresIn: config.jwt_access_expires_in } as SignOptions,
+  );
+
+  const refreshToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_refresh_secret!,
+    { expiresIn: config.jwt_refresh_expires_in } as SignOptions,
+  );
+
+  return { accessToken, refreshToken };
 };
 
 export const authService = { loginUser };
